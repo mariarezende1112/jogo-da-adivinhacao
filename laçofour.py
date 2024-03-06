c4b022eeafe5d8936a6ebb5380cd1bfa5decf19e
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-03-06 09:54:41 -0300

    Add files via upload

---
+++ laçofour.py
@@ -0,0 +1,21 @@
+nomes = ['joão','Pedro','Letícia']
+for nome in nomes:
+    print(nome)
+
+print("#################")
+
+
+
+soma = 0
+for i in range(1,11):
+    soma=soma +1
+    print(soma)
+
+print("*******")    
+
+
+n = 5
+fatorial = 1
+for i in range(1, n+1):
+    fatorial = fatorial * i 
+    
