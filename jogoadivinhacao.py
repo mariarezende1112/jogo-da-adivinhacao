db03444fb31c0a120554769f2bfffbdd25a92202
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-04-17 09:00:25 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -5,14 +5,18 @@ print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
 print('*********************************')
 
 #definindo o numero secreto
-numeroSecreto = round(random.random()*100)
+numeroSecreto = round(random.random()*1,101)
 #print(numeroSecreto)
 
 rodada = 1
 #definindi numero de de tentativas
 numerotentativas = 10
 
-while(rodada <= 3):
+print("Qual o nível de dificuldade?")
+print("(1)-Fácil, (2)-Médio, (3)-Difícil")
+
+nível = int(input('Defina '))
+while(rodada <= numerotentativas):
     print('tentativas',rodada,'de',numerotentativas)
     
 
@@ -30,6 +34,6 @@ while(rodada <= 3):
     elif(chute>numeroSecreto):
         print('Você errou! O numero secreto é um numero menor')
     else:
-        print('Você errou! O numero secreto é um numero menor')    
+        print('Você errou! O numero secreto é um numero maior')    
     #numerotentativas = numerotentativas - 1
     rodada = rodada + 1    
